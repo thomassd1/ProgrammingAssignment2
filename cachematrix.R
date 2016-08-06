11bc444acfb897e43e001b12d082846c47903189
thomassd <thomas.steved@gmail.com>
2016-08-06 12:09:09 -0400

    Revert "Programming Assignment 2: Lexical Scoping"
    This reverts commit 8c8878c7520cc9a3d4711b021ee6490d239a72d7.

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,15 @@
-## makeCacheMatrix.R
-## 08-06-2016 steveT
-## This function creates a special "matrix" object that can cache its inverse.
-## Usage example:
-## myMatrix <- makeCacheMatrix()
-## myMatrix$set(matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2))
-## myMatrix$get()
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function (y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <-function() x
-  setmatr <- function(solve) inv <<- solve
-  getmatr <- function() inv
-  list (set=set, get=get, setmatr=setmatr, getmatr=getmatr)
+
 }
 
 
-## This function computes the inverse of the special "matrix" returned 
-## by makeCacheMatrix above. If the inverse has already been calculated 
-##(and the matrix has not changed), then the cachesolve should retrieve 
-## the inverse from the cache.
-## Usage example:
-## cacheSolve(myMatrix)
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-  inv <- x$getmatr()
-  if(!is.null(inv)){
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data,...)
-  x$setmatr(inv)
-  inv
-  ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
 }
